caccc5f68831dce296aa2df9bc7a07a16b574eed
not-meet <mjmeet.279@gmail.com>
2024-10-05 06:47:53 +0530

    added the expense page draft

---
+++ app/pages/expenses.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+const expenses = () => {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-black">
+      <div className="grid min-h-screen w-full grid-col-10 grid-rows-10 gap-4 p-6 rounded-lg">
+        <div className="col-span-10 rounded-lg bg-gray-900">..</div>
+        <div className="col-span-3 row-span-6 rounded-lg bg-gray-900">..</div>
+        <div className="col-span-7 row-span-7 bg-gray-900 rounded-lg">..</div>
+        <div className="col-span-3 row-span-3 bg-gray-900 rounded-lg">..</div>
+        <div className="col-span-4 row-span-2 bg-gray-900 rounded-lg">..</div>
+        <div className="col-span-3 row-span-2 bg-gray-900 rounded-lg">..</div>
+      </div>
+    </div>
+  )
+}
+
+export default expenses;
